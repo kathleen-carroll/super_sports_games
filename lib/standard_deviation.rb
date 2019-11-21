96360690c76510962fb09273f77672d3ce10c815
Kathleen Carroll <khakicarroll@comcast.net>
2019-11-21 13:33:07 -0700

    first updates to std dev

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,21 @@
 ages = [24, 30, 18, 20, 41]
+sum = 0
 
 # Your code here for calculating the standard deviation
+ages.each do |age|
+  sum += age
+end
+
+p sum
+
+p sum2 = ages.sum
+
+p count = ages.count
+
+#p ages.find_index(age)
+
+ages.each do |age|
+  p ages.find_index(age)
+end
 
 # When you find the standard deviation, print it out
