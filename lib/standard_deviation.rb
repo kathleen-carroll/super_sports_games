340cd8cfb3ddd783d7048a5e269cdc5c307ee3dd
Kathleen Carroll <khakicarroll@comcast.net>
2019-11-21 13:45:01 -0700

    making more progress on iteration 1

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -8,14 +8,18 @@ end
 
 p sum
 
-p sum2 = ages.sum
+#p sum2 = ages.sum
+#p count = ages.count
+#p ages[-1]
+count = ages.find_index(ages[-1]) + 1
 
-p count = ages.count
+# ages.each do |age|
+#   p ages.find_index(age)
+# end
 
-#p ages.find_index(age)
+#finding the average 
+average = (sum * 1.0) / count
 
-ages.each do |age|
-  p ages.find_index(age)
-end
+p average
 
 # When you find the standard deviation, print it out
